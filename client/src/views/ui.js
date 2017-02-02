2b90cc43e8e63952b2b35dfa0cdbbe22fba455bf
John Campbell <j.a.campbell@icloud.com>
2017-02-02 15:47:51 +0000

    added app.js and ui.js

---
+++ client/src/views/ui.js
@@ -0,0 +1,28 @@
+var UI = function () {
+    this.container = document.body;
+    this.render();
+}
+
+UI.prototype = {
+    // createContainer: function () {
+    //     newContainer = document.createElement('div');
+
+    //     var classList = Array.from(arguments);
+    //     classList.forEach(function (cssClass) {
+    //         newContainer.classList.add(cssClass);
+    //     });
+
+    //     this.container.appendChild(newContainer);
+    //     return newContainer;
+    // },
+
+    render: function () {
+        // var headerContainer = this.createContainer('header', 'flex', 'center', 'shadow');
+        // new Header(headerContainer);
+        // var searchBar = new SearchBar(headerContainer);
+
+        // var imageContainer = this.createContainer('image-container', 'flex', 'wrap', 'center');
+        // var imageDisplay = new ImageDisplay(imageContainer);
+        // searchBar.setImageContainer(imageDisplay);
+    }
+}
