29ee4c9fe3a97df5f7b7efe5744f09d685ef6de0
John Campbell <j.a.campbell@icloud.com>
2017-02-03 13:11:42 +0000

    added ajax module to send requests

---
+++ client/src/helpers/ajax.js
@@ -0,0 +1,20 @@
+var ajax = {
+   
+    get: function (url, callback) {
+        var request = new XMLHttpRequest();
+        request.open('GET', url);
+        
+        request.onload = function () {
+            if (this.status !== 200) {
+                console.error('Request status:', this.status);
+                return;
+            }
+
+            var jsonString = this.responseText;
+            var data = JSON.parse(jsonString).data;
+            callback(data);
+        }
+        
+        request.send();   
+    }
+}
---
+++ client/src/views/memoView.js
@@ -0,0 +1,7 @@
+var MemoView = function(container){
+
+};
+
+MemoView.prototype = {
+  
+};
