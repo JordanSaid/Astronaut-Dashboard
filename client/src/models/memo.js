b9ac5e118a6df30528506cb67c242e7a7f5455c3
John Campbell <j.a.campbell@icloud.com>
2017-02-03 11:23:27 +0000

    tests for film.js passing x 4

--- client/src/models/memo.js
+++ client/src/models/memo.js
@@ -2,10 +2,10 @@ var Memo = function(options){
   this.title = options.title;
   this.body = options.body;
   this.emoji = options.emoji;
-}
+};
 
 Memo.prototype = {
- 
+
 };
 
 module.exports = Memo;
--- client/src/models/specs/memo_test.js
+++ client/src/models/specs/memo_test.js
@@ -1,17 +1,35 @@
 var assert = require("assert");
-var memo = require("./memo");
+var Memo = require("../memo");
 
+var memo;
 var options = {
   title: "My memo",
-  body: "Hello diary"
+  body: "Hello diary",
   emoji: {
-    name: "smiley"
+    name: "smiley",
     url: "urlforsmiley"
   }
 };
 
-Describe ("Memo", function(){
+describe ("Memo", function(){
+
   beforeEach(function(){
     memo = new Memo(options);
-  })
+  });
+
+  it("should have a name",function(){
+    assert.equal("My memo",memo.title);
+  });
+  
+  it("should have a body",function(){
+    assert.equal("Hello diary", memo.body);
+  });
+
+  it("should have an emoji name",function(){
+    assert.equal("smiley",memo.emoji.name);
+  });
+
+  it("should have an emoji url",function(){
+    assert.equal("urlforsmiley",memo.emoji.url);
+  });
 })
