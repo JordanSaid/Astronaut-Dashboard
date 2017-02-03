4ecad8037b6e1d39a47f4f259e32e349c2fe410a
John Campbell <j.a.campbell@icloud.com>
2017-02-03 15:15:56 +0000

    added memo dashboard and some styling

---
+++ client/src/models/mapWrapper.js
@@ -0,0 +1,24 @@
+var MapWrapper = function(container, coordinates, zoom) {
+  var container = document.querySelector("#right");
+  this.googleMap = new google.maps.Map(container, {
+    center: coordinates,
+    zoom: zoom
+  });
+}
+
+MapWrapper.prototype = {
+  addMarker: function(coordinates) {
+    var marker = new google.maps.Marker({
+      position: coordinates,
+      map: this.googleMap,
+    });
+      this.googleMap.setCenter(coordinates)
+  },
+  setCurrentLocation: function(position) {
+      var pos = {lat: position.coords.latitude, lng: position.coords.longitude};
+      this.googleMap.setCenter(pos);
+      this.googleMap.setZoom(15);
+  },
+}
+
+module.exports = MapWrapper;
--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -1,5 +1,10 @@
 var News = require('../models/news');
+<<<<<<< HEAD
 var MemoView = require('./memoView')
+=======
+var MapWrapper = require('../models/MapWrapper');
+var SpaceStation = require('../models/spaceStation');
+>>>>>>> 5a704ac265d0fcd98b08a422955b06aec0e29210
 
 var UI = function () {
     this.news = new News();
@@ -8,6 +13,11 @@ var UI = function () {
         console.log(headlineArray)
     }.bind(this))
     this.container = document.body;
+
+    this.spaceStation = new SpaceStation();
+    this.spaceStation.all(function(detailsArray) {
+        this.renderMap(detailsArray);
+    }.bind(this))
 
 }
 
@@ -28,12 +38,19 @@ UI.prototype = {
         var headlines = document.querySelector('#ticker');
            headlinesArray.forEach(function(headline) {
              var headlineTitle = document.createElement("p");
-             headlineTitle.innerHTML = "<a href='" + headline.url + "'>" + headline.title + "</a>";
+             headlineTitle.innerHTML = "<a href='" + headline.url + "'>" + "<marquee direction='left'>" + headline.title + "</marquee></a>";
              headlines.appendChild(headlineTitle);
            });
+<<<<<<< HEAD
         var leftDiv = document.querySelector("#left");
         var memoView = new MemoView(leftDiv);
         memoView.renderMemoDash();
+=======
+
+        // var leftDiv = document.querySelector("left-div");
+        // var memoView = new MemoView(leftDiv);
+        // memoView.renderMemoDash();
+>>>>>>> 5a704ac265d0fcd98b08a422955b06aec0e29210
      
         // var headerContainer = this.createContainer('header', 'flex', 'center', 'shadow');
         // new Header(headerContainer);
@@ -42,6 +59,14 @@ UI.prototype = {
         // var imageContainer = this.createContainer('image-container', 'flex', 'wrap', 'center');
         // var imageDisplay = new ImageDisplay(imageContainer);
         // searchBar.setImageContainer(imageDisplay);
+    },
+    renderMap: function (detailsArray) {
+        var mapDiv = document.querySelector('#right');
+        var stationLat = detailsArray.iss_position.latitude;
+        var stationLon = detailsArray.iss_position.longitude;
+        var spaceStationLocation = {lat: parseInt(stationLat), lng: parseInt(stationLon)};
+        this.mapWrapper = new MapWrapper(mapDiv, spaceStationLocation, 4);
+        this.mapWrapper.addMarker(spaceStationLocation);
     }
 
 
